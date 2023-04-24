6f2c5a6d11fa4571a2a6aff72ad7f52ef3282b86
Simon Mundy <simon.mundy@peptolab.com>
2023-04-24 18:07:20 +1000

    Refactored package-specific helpers, added BC plugin RichContent

---
+++ src/Helper/RichContent.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Contenir\View\Helper;
+
+use Laminas\View\Helper\AbstractHelper;
+
+class RichContent extends AbstractHelper
+{
+    protected $template = [
+        'outerTag' => 'section',
+        'outerClass' => 'grid grid--content',
+        'innerTag' => '',
+        'innerClass' => '',
+        'columnTag' => 'div',
+        'columnClass' => [
+            'default' => 'grid__col',
+            2 => 'grid__col grid__col--2',
+            3 => 'grid__col grid__col--3'
+        ]
+    ];
+
+    public function __invoke($content, array $template = [])
+    {
+        $template = array_merge($this->template, $template);
+        $html = '';
+
+        $sections = preg_split('/<(\w+)([^>]*)>([^<]*)__SECTION__([^<]*)<\/\1>/msi', $content, -1, PREG_SPLIT_NO_EMPTY);
+        foreach ($sections as $section) {
+            $html .= $this->formatSection($section, $template);
+        }
+
+        return $html;
+    }
+
+    protected function formatSection($section, $template)
+    {
+        extract($template);
+
+        $html = '';
+        $columns = preg_split('/<(\w+)([^>]*)>([^<]*)__COL__([^<]*)<\/\1>/msi', $section, -1, PREG_SPLIT_NO_EMPTY);
+        $columnCnt = count($columns);
+        $columnClass = $columnClass[$columnCnt] ?? $columnClass['default'] ?? null;
+
+        foreach ($columns as $column) {
+            $tagClass = ($columnClass) ? sprintf(' class="%s"', $this->view->EscapeHtml($columnClass)) : '';
+            $tagStart = ($columnTag) ? sprintf('<%s%s>', $columnTag, $tagClass) : '';
+            $tagEnd = ($columnTag) ? sprintf('</%s>', $columnTag) : '';
+            $html .= sprintf('%s%s%s', $tagStart, trim($column), $tagEnd);
+        }
+
+        $tagClass = ($innerClass) ? sprintf(' class="%s"', $this->view->EscapeHtml($innerClass)) : '';
+        $tagStart = ($innerTag) ? sprintf('<%s%s>', $innerTag, $tagClass) : '';
+        $tagEnd = ($innerTag) ? sprintf('</%s>', $innerTag) : '';
+        $html = sprintf('%s%s%s', $tagStart, trim($html), $tagEnd);
+
+        $tagClass = ($outerClass) ? sprintf(' class="%s"', $this->view->EscapeHtml($outerClass)) : '';
+        $tagStart = ($outerTag) ? sprintf('<%s%s>', $outerTag, $tagClass) : '';
+        $tagEnd = ($outerTag) ? sprintf('</%s>', $outerTag) : '';
+        $html = sprintf('%s%s%s', $tagStart, trim($html), $tagEnd);
+
+        return $html;
+    }
+}
--- src/Module.php
+++ src/Module.php
@@ -2,7 +2,6 @@
 
 namespace Contenir\View;
 
-use Contenir\Asset\AssetManager;
 use Laminas\ServiceManager\Factory\InvokableFactory;
 
 class Module
@@ -17,74 +16,53 @@ class Module
         return [
             'view_helpers' => [
                 'aliases' => [
-                    'acl'             => Helper\Acl::class,
-                    'Acl'             => Helper\Acl::class,
-                    'asset'           => Helper\Asset::class,
-                    'Asset'           => Helper\Asset::class,
-                    'assetSrc'        => Helper\AssetSrc::class,
-                    'AssetSrc'        => Helper\AssetSrc::class,
-                    'assetSrcSet'     => Helper\AssetSrcSet::class,
-                    'AssetSrcSet'     => Helper\AssetSrcSet::class,
-                    'assetUrl'        => Helper\AssetUrl::class,
-                    'AssetUrl'        => Helper\AssetUrl::class,
-                    'cache'           => Helper\Cache::class,
-                    'Cache'           => Helper\Cache::class,
-                    'collection'      => Helper\Collection::class,
-                    'Collection'      => Helper\Collection::class,
-                    'dateFormat'      => Helper\DateFormat::class,
-                    'DateFormat'      => Helper\DateFormat::class,
-                    'escapeEmail'     => Helper\EscapeEmail::class,
-                    'EscapeEmail'     => Helper\EscapeEmail::class,
-                    'fileSize'        => Helper\FileSize::class,
-                    'FileSize'        => Helper\FileSize::class,
-                    'fileType'        => Helper\FileType::class,
-                    'FileType'        => Helper\FileType::class,
-                    'formGroup'       => Helper\FormGroup::class,
-                    'FormGroup'       => Helper\FormGroup::class,
-                    'icon'            => Helper\Icon::class,
-                    'Icon'            => Helper\Icon::class,
-                    'image'           => Helper\Image::class,
-                    'Image'           => Helper\Image::class,
-                    'resource'        => Helper\Resource::class,
-                    'Resource'        => Helper\Resource::class,
-                    'resourceContent' => Helper\ResourceContent::class,
-                    'ResourceContent' => Helper\ResourceContent::class,
-                    'resourceMeta'    => Helper\ResourceMeta::class,
-                    'ResourceMeta'    => Helper\ResourceMeta::class,
-                    'settings'        => Helper\Settings::class,
-                    'Settings'        => Helper\Settings::class,
-                    'socialLink'      => Helper\SocialLink::class,
-                    'SocialLink'      => Helper\SocialLink::class,
-                    'srcset'          => Helper\Srcset::class,
-                    'Srcset'          => Helper\Srcset::class,
-                    'truncate'        => Helper\Truncate::class,
-                    'Truncate'        => Helper\Truncate::class,
-                    'urlFormat'       => Helper\UrlFormat::class,
-                    'UrlFormat'       => Helper\UrlFormat::class,
+                    'acl'         => Helper\Acl::class,
+                    'Acl'         => Helper\Acl::class,
+                    'cache'       => Helper\Cache::class,
+                    'Cache'       => Helper\Cache::class,
+                    'dateFormat'  => Helper\DateFormat::class,
+                    'DateFormat'  => Helper\DateFormat::class,
+                    'escapeEmail' => Helper\EscapeEmail::class,
+                    'EscapeEmail' => Helper\EscapeEmail::class,
+                    'fileSize'    => Helper\FileSize::class,
+                    'FileSize'    => Helper\FileSize::class,
+                    'fileType'    => Helper\FileType::class,
+                    'FileType'    => Helper\FileType::class,
+                    'formGroup'   => Helper\FormGroup::class,
+                    'FormGroup'   => Helper\FormGroup::class,
+                    'icon'        => Helper\Icon::class,
+                    'Icon'        => Helper\Icon::class,
+                    'image'       => Helper\Image::class,
+                    'Image'       => Helper\Image::class,
+                    'richContent' => Helper\RichContent::class,
+                    'RichContent' => Helper\RichContent::class,
+                    'settings'    => Helper\Settings::class,
+                    'Settings'    => Helper\Settings::class,
+                    'socialLink'  => Helper\SocialLink::class,
+                    'SocialLink'  => Helper\SocialLink::class,
+                    'srcset'      => Helper\Srcset::class,
+                    'Srcset'      => Helper\Srcset::class,
+                    'truncate'    => Helper\Truncate::class,
+                    'Truncate'    => Helper\Truncate::class,
+                    'urlFormat'   => Helper\UrlFormat::class,
+                    'UrlFormat'   => Helper\UrlFormat::class,
                 ],
                 'factories' => [
-                    Helper\Acl::class             => Helper\Factory\AclFactory::class,
-                    Helper\Asset::class           => Helper\Factory\AssetFactory::class,
-                    Helper\AssetSrc::class        => Helper\Factory\AssetSrcSetFactory::class,
-                    Helper\AssetSrcSet::class     => Helper\Factory\AssetSrcSetFactory::class,
-                    Helper\AssetUrl::class        => Helper\Factory\AssetUrlFactory::class,
-                    Helper\Cache::class           => Helper\Factory\CacheFactory::class,
-                    Helper\Collection::class      => Helper\Factory\CollectionFactory::class,
-                    Helper\DateFormat::class      => InvokableFactory::class,
-                    Helper\EscapeEmail::class     => InvokableFactory::class,
-                    Helper\FileSize::class        => InvokableFactory::class,
-                    Helper\FileType::class        => InvokableFactory::class,
-                    Helper\FormGroup::class       => InvokableFactory::class,
-                    Helper\Icon::class            => InvokableFactory::class,
-                    Helper\Image::class           => Helper\Factory\ImageFactory::class,
-                    Helper\Resource::class        => Helper\Factory\ResourceFactory::class,
-                    Helper\ResourceContent::class => InvokableFactory::class,
-                    Helper\ResourceMeta::class    => InvokableFactory::class,
-                    Helper\Settings::class        => Helper\Factory\SettingsFactory::class,
-                    Helper\SocialLink::class      => InvokableFactory::class,
-                    Helper\Srcset::class          => InvokableFactory::class,
-                    Helper\Truncate::class        => InvokableFactory::class,
-                    Helper\UrlFormat::class       => InvokableFactory::class,
+                    Helper\Acl::class         => Helper\Factory\AclFactory::class,
+                    Helper\Cache::class       => Helper\Factory\CacheFactory::class,
+                    Helper\DateFormat::class  => InvokableFactory::class,
+                    Helper\EscapeEmail::class => InvokableFactory::class,
+                    Helper\FileSize::class    => InvokableFactory::class,
+                    Helper\FileType::class    => InvokableFactory::class,
+                    Helper\FormGroup::class   => InvokableFactory::class,
+                    Helper\Icon::class        => InvokableFactory::class,
+                    Helper\Image::class       => Helper\Factory\ImageFactory::class,
+                    Helper\RichContent::class => InvokableFactory::class,
+                    Helper\Settings::class    => Helper\Factory\SettingsFactory::class,
+                    Helper\SocialLink::class  => InvokableFactory::class,
+                    Helper\Srcset::class      => InvokableFactory::class,
+                    Helper\Truncate::class    => InvokableFactory::class,
+                    Helper\UrlFormat::class   => InvokableFactory::class,
                 ],
             ],
             'view_manager' => [
@@ -97,31 +75,6 @@ class Module
                 'strategies'               => [
                     'ViewJsonStrategy',
                 ]
-            ],
-            'view_asset' => [
-                'asset_manager' => AssetManager::class
-            ],
-            'view_srcset' => [
-                'sizes' => [
-                    'small' => [
-                        640  => 320,
-                        960  => 480,
-                        1440 => 720
-                    ],
-                    'medium' => [
-                        640  => 480,
-                        960  => 720,
-                        1440 => 1080,
-                        2400 => 1800
-                    ],
-                    'large' => [
-                        640  => 640,
-                        960  => 960,
-                        1440 => 1440,
-                        2400 => 2400
-                    ]
-                ],
-                'helper' => '/usr/local/bin/convert'
             ]
         ];
     }
